9d546105c9a7f91ad3682e6bb509c5fb32a9de06
namnd <namsupermen06@gmail.com>
2024-08-26 21:17:01 +0700

    jwt api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from './config/typeorm.config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { AuthModule } from './modules/auth/auth.module';\
+import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { ApiTokenCheckMiddleware } from './common/middleware/api-token-check.middleware';
 
 @Module({
   imports: [
@@ -26,4 +32,3 @@ export class AppModule implements NestModule {
       .forRoutes({ path: '/', method: RequestMethod.ALL });
   }
 }
-
