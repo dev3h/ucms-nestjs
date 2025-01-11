1ea37306b17d8bf3afbcc10653afd5bedf4533ac
namnd <namnd@rabiloo.com>
2025-01-11 12:03:45 +0700

    validate password with value vietnamese

--- src/modules/auth/dto/email.dto.ts
+++ src/modules/auth/dto/email.dto.ts
@@ -9,17 +9,17 @@ export class EmailRequestDto {
     example: 'namnd@yopmail.com',
   })
   @IsExists({ tableName: 'users', column: 'email' })
+  @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+  @IsEmail(
+    {},
+    { message: (args) => I18nContext.current().t('validation.email') },
+  )
   @MaxLength(50, {
     message: (args) =>
       I18nContext.current().t('validation.maxLength', {
         args: { column: 'Email', max: 50 },
       }),
   })
-  @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
-  @IsEmail(
-    {},
-    { message: (args) => I18nContext.current().t('validation.email') },
-  )
   @IsNotEmpty({
     message: (args) => I18nContext.current().t('validation.isNotEmpty'),
   })
